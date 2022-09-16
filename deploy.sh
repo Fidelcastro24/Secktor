6fc8d5e7575d557e3b93488c58151dcaf163e1ce
Fidel Castro <74963732+Fidelcastro24@users.noreply.github.com>
2022-09-16 05:20:35 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ apt-get install nodejs -y
 apt-get install ffmpeg -y
 apt-get install imagemagick -y
 apt-get install yarn
-git clone https://github.com/SamPandey001/Secktor-Md
+git clone https://github.com/Fidelcastro24/Secktor
 cd Secktor-Md
 npm start 
